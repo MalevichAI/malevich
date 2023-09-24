041e20cd41c1d31dbfbddae55b7ca6dcb130d21d
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-09-24 22:29:07 +0300

    add: autoflow enginge; upd: requirements

--- malevich/_autoflow/manager.py
+++ malevich/_autoflow/manager.py
@@ -1,22 +1,42 @@
 from typing import Iterable
 
-from malevich._autoflow.tracer import ExecutionTree
+from malevich._autoflow.singleton import SingletonMeta
+from malevich._autoflow.tree import ExecutionTree
 
 
-class FlowManager:
+class Flow:
+    """Orchestrate the execution of the pipeline"""
+
+    # Internal representation of the execution tree
     __flow = None
+
     def isinflow() -> bool:
-        return FlowManager.__flow is not None
+        """Check if the current execution is inside a flow"""
+        return Flow.__flow is not None
 
     def flow_ref() -> ExecutionTree:
-        return FlowManager.__flow
+        """Returns the reference to the current flow in the context"""
+        return Flow.__flow
 
 
-    def __enter__(self) -> None:
-        assert FlowManager.__flow is None, "Flow already exists"
-        FlowManager.__flow = ExecutionTree()
+    def __enter__(self) -> ExecutionTree:
+        assert Flow.__flow is None, "Flow already exists"
+        Flow.__flow = ExecutionTree()
+        return Flow.__flow
 
 
     def __exit__(self, *args: Iterable) -> None:
-        print(FlowManager.__flow.tree)
-        FlowManager.__flow = None
+        Flow.__flow = None
+
+
+class Registry(metaclass=SingletonMeta):
+    _registry = {}
+
+    def register(self, key: str, value: object) -> None:
+        self._registry[key] = value
+
+    def get(self, key: str) -> object:
+        return self._registry[key]
+
+    def __getitem__(self, key: str) -> object:
+        return self._registry[key]
