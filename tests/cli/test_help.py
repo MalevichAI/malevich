c1cbe50c0a5a429ac8458ad14d4ff732e44ef8da
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2024-01-03 15:39:34 +0500

    add: tests env, first test

---
+++ tests/cli/test_help.py
@@ -0,0 +1,10 @@
+from typer.testing import CliRunner
+from malevich.cli import app
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(app, ['--help'])
+    assert result.exit_code == 0
+    assert len(result.output) > 0
+    
