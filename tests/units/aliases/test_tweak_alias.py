c54618fb8afcf42fc0afa1a355a4453ad86e52ce
RamPrin <pak55256@gmail.com>
2024-03-04 13:07:50 +0300

    added alias tests

--- tests/units/aliases/test_tweak_alias.py
+++ tests/units/aliases/test_tweak_alias.py
@@ -1,3 +1,4 @@
+from malevich.models.task.promised import PromisedTask
 from malevich.models.flow_function import FlowFunction
 from malevich.models.results.base import BaseResult
 from malevich.testing import FlowTestEnv, FlowTestSuite, ImageDependency
@@ -18,18 +19,41 @@ class TestAlias(FlowTestSuite):
 
         data = collection(
             name='test_col1',
+            alias='test1_collection',
             df=table(
-                [1,2,3,4,5],
+                [[1,2,3,4,5]],
                 columns=['A', 'B', 'C', 'D', 'E']
             )
         )
 
-        data1 = run(rename(data, config(A='F')), alias='rename1')
+        data1 = collection(
+            name='test_col2',
+            alias='test2_collection',
+            df=table(
+                [[1,2,3,4,5]],
+                columns=['X', 'B', 'C', 'D', 'Y']
+            )
+        )
 
-        data2 = run(add_column(data, config(column='Z', value=10)), alias='add_column1')
+        data2 = run(rename(data, config(A='F')), alias='rename1')
 
-        return run(merge(data, data1, data2), alias='merge1')
+        data3 = run(add_column(data1, config(column='Z', value=10)), alias='add_column1')
+
+        return run(merge(data, data1, data2, data3), alias='merge1')
     
     @staticmethod
-    def on_result(flow: PromisedTask, results) -> None:
-        flow.get_operations()
+    def on_interpretation(task: PromisedTask) -> None:
+        ops = task.get_operations()
+        assert 'rename1' in ops
+        assert 'add_column1' in ops
+        assert 'merge1' in ops
+
+        injs = task.get_injectables()
+        inj_assert = []
+        inj_values = [('test2_collection', 'test_col2'), ('test1_collection', 'test_col1')]
+        
+        for inj in injs:
+            inj_assert.append((inj.get_inject_key(), inj.get_inject_data()))
+        
+        for val in inj_values:
+            assert val in inj_assert
