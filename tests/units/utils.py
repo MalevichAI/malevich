dda27ebbc6fcdf57571866369f83adcd702b63b5
RamPrin <pak55256@gmail.com>
2024-03-11 17:12:19 +0300

    tests updated

--- tests/units/utils.py
+++ tests/units/utils.py
@@ -8,14 +8,24 @@ from malevich.manifest import manf
 
 def assert_installed_package(package_name: str, installer: str) -> tuple[str, str]:
     try:
-        module_path = importlib.import_module(
-            f'malevich.{package_name}'
-        ).__file__
-
-        stub_path = os.path.join(
-            os.path.dirname(module_path), '__init__.py'
+        module_path = os.path.dirname(
+            importlib.import_module(
+                'malevich'
+            ).__file__
         )
-
+        for _, dirs, _ in os.walk(module_path):
+            if package_name in dirs:
+                module_path = os.path.join(module_path, package_name)
+                for _, _, files in os.walk(module_path):
+                    if 'F.py' in files:
+                        stub_path = os.path.join(module_path, 'F.py')
+                        break
+                    else:
+                        raise ImportError
+                break
+            else:
+                raise ImportError
+        importlib.import_module('malevich.' + package_name + ".F")
     except ImportError:
         raise AssertionError(
             f'{package_name} is not importable'
@@ -33,17 +43,15 @@ def assert_installed_package(package_name: str, installer: str) -> tuple[str, st
         'installer'
     ) == installer, 'Wrong installer'
 
-    with open(stub_path) as stub_file:
-        _ = stub_file.read()
-        assert len(
-            Registry().registry
-        ) > 0, 'Package has not registered any operations'
+    assert len(
+        Registry().registry
+    ) > 0, 'Package has not registered any operations'
 
     return module_path, stub_path
 
 
 def wipe_installed_package(module_path: str, stub_path: str, package_name: str) -> None:
-    shutil.rmtree(os.path.dirname(module_path))
+    shutil.rmtree(os.path.dirname(stub_path))
     manf.remove('dependencies', package_name)
 
     assert not os.path.exists(stub_path), 'Stub not removed'
