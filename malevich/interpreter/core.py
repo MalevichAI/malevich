f0e4fbfdafa2d2a7096c760ba683785f21f1e61d
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2024-04-04 21:28:02 +0300

    fix: cherrypicked merge with collections

--- malevich/interpreter/core.py
+++ malevich/interpreter/core.py
@@ -325,7 +325,7 @@ class CoreInterpreter(Interpreter[CoreInterpreterState, CoreTask]):
                     **state.cfg.collections,
                     f"{coll}": uploaded_core_id,
                 }
-                state.extra_colls[op.uuid][link.name] = coll
+                state.extra_colls[op.uuid][link.name].append(coll)
 
             if not op.alias:
                 op.alias = extra["processor_id"] + '-' + str(_name(extra["processor_id"]))  # noqa: E501
