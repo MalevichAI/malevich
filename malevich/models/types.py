3b37ce71cb5921774be0c9f84dd134c0c6687cd9
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2024-06-11 15:32:02 +0300

    upd: strict python strs and assets inferfaces

--- malevich/models/types.py
+++ malevich/models/types.py
@@ -4,9 +4,10 @@ from .._autoflow.tracer import traced
 from .._autoflow.tree import ExecutionTree
 from .argument import ArgumentLink
 from .nodes.base import BaseNode
+from .python_string import PythonString  # noqa: F401 -- imported for type hinting
 
 Nodes = Iterable[BaseNode]
 TracedNode = traced[BaseNode]
 TracedNodes = Iterable[traced[BaseNode]]
 FlowOutput = Union[TracedNode, TracedNodes, None]
-FlowTree = ExecutionTree[BaseNode, ArgumentLink]
+FlowTree = ExecutionTree[BaseNode, ArgumentLink[BaseNode]]
