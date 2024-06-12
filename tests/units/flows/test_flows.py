6446125d7d36b0e87d9b35e67ee357c8ee2571b2
RamPrin <pak55256@gmail.com>
2024-06-12 17:34:17 +0300

    isolated test

--- tests/units/flows/test_flows.py
+++ tests/units/flows/test_flows.py
@@ -121,7 +121,7 @@ class TestSpaceFlow(FlowTestSuite):
             "utility": SpaceDependency(package_id="utility", options=SpaceDependencyOptions(reverse_id='utility')),
         }
     )
-    interpreter = SpaceInterpreter(version_mode=VersionMode.PATCH)
+    interpreter = SpaceInterpreter(version_mode=VersionMode.MINOR)
 
     @flow
     def space_flow():
