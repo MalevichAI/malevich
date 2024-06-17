4d31a3d51677b8a0aa3f6b1c85effcb344643b65
RamPrin <pak55256@gmail.com>
2024-06-17 14:07:42 +0300

    tests update

--- tests/app_build/test_platforms.py
+++ tests/app_build/test_platforms.py
@@ -29,7 +29,6 @@ def test_space_platform():
     assert len(res.columns) == 1 and res.columns[0] == 'result'
     task1 = Space(
         reverse_id='malevich_platform_test_space',
-        force_attach=True,
         deployment_id=task.state.aux.task_id
     )
     assert task.state.aux.task_id == task1.state.aux.task_id
@@ -38,15 +37,13 @@ def test_space_platform():
     assert len(res.columns) == 1 and res.columns[0] == 'result'
     task2 = Space(
         reverse_id='malevich_platform_test_space',
-        force_attach=True,
-        attach_to_last=True
+        attach_to_any=True
     )
     assert task.state.aux.task_id == task2.state.aux.task_id
     task2.run()
     res = task2.results()[0].get_df()
     assert len(res.columns) == 1 and res.columns[0] == 'result'
     task1.stop()
-    task2.stop()
 
 def test_core_platform():
     task = Core(platform_core_flow, user=os.environ.get('CORE_USER'), access_key=os.environ.get('CORE_PASS'))
